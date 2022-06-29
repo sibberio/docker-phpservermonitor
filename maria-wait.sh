8ac91b01ba8783fc65d9bf1280f6dd9e388d2911
sibberio <saverio.cannara@gmail.com>
2022-06-29 14:36:39 +0200

    Update maria-wait.sh

--- maria-wait.sh
+++ maria-wait.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
+
 set -ueo pipefail
 
 echo -n "Waiting until MariaDB has started"
-until (timeout 1 bash -c "< /dev/tcp/db/3306") &> /dev/null; do
 until (timeout 1 bash -c "< /dev/tcp/${MYSQL_HOST}/3306") &> /dev/null; do
 	echo -n .
 	sleep 1
